8dc5b4db5db43005e1c7e7fed69facf71739f9cf
Skyber_2 <arttim1019@gmail.com>
2024-02-24 01:29:11 +0300

    start working on calculator of ahieves

--- calculator/static/calculator_id/js/calculator_id.js
+++ calculator/static/calculator_id/js/calculator_id.js
@@ -1,104 +1,20 @@
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
-
-let selectedAchievements = {}
-
-
-$.get('/get_achievements/', function(data) {
-    const divCategories = document.getElementById("categories");
-    for (let category in data) {
-        const divNavItem = document.createElement("div");
-        divNavItem.textContent = category;
-        divNavItem.onclick = function() {
-            populateAchievements(category, data[category]);
-        };
-        divNavItem.className = 'nav-item'
-        divCategories.appendChild(divNavItem);
-    }
-});
-
-
-function populateAchievements(category, achievements) {
-    const divAchievements = document.getElementById('achievements')
-    divAchievements.innerHTML = ''
-
-    for (let achievement of achievements) {
-        achievement = achievement[0].toUpperCase() + achievement.slice(1)
-        const divPotomok = document.createElement('div')
-        const label = document.createElement('label')
-        label.className = 'label-achieve'
-
-        const input = document.createElement('input')
-        input.className = 'input-achieve'
-        input.type = 'checkbox'
-        input.name = 'achievement'
-        input.value = achievement
-        if (selectedAchievements[achievement]) {
-            input.checked = true
-        }
-        input.onchange = function() {
-            const statusSelect = this.nextElementSibling
-            if (this.checked) {
-                statusSelect.style.display = 'inline-block'
-                selectedAchievements[achievement] = statusSelect.value
-            } else {
-                statusSelect.style.display = 'none'
-                delete selectedAchievements[achievement]
-            }
-        }
-
-        const select = document.createElement('select')
-        select.className = 'select-achieve'
-        select.style.display = 'none'
-        select.options.add(new Option("Участник", "participant"))
-        select.options.add(new Option("Призер", "prizeWinner"))
-        select.options.add(new Option("Победитель", "winner"))
-        select.options.add(new Option("Лауреат", "laureate"))
-        select.onchange = function() {
-            selectedAchievements[achievement] = this.value
-        }
-
-        if (selectedAchievements[achievement]) {
-            select.style.display = 'inline-block'
-            select.value = selectedAchievements[achievement]
-            input.checked = true
-        }
-
-        label.appendChild(input);
-        label.appendChild(document.createTextNode(' ' + achievement));
-        label.appendChild(select);
-        divPotomok.appendChild(label)
-        divAchievements.appendChild(divPotomok)
-    }
-}
-
-
-document.getElementById('calculate-btn').addEventListener('click', function() {
+function getListAchieves(razdel) {
     $.ajax({
-        url: '/send_achieves/',
-        type: 'POST',
-        data: JSON.stringify(selectedAchievements),
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json',
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader('X-CSRFToken', getCookie('csrftoken'));
+        url: '/get_achievements/',
+        type: 'post',
+        data: {
+            'razdel': razdel
         },
-        success: function(response) {
-            console.log(response);
-        }
-    });
-});
+        success: function(achieveList) {
+            const sendBtn = document.getElementById('calculate-btn')
+            const achievements = document.getElementById('achievements')
 
+            sendBtn.style.display = 'block'
+            achievements.innerHTML = ''
+            achievements.innerHTML = achieveList
+        },
+        error: function() {
+            // alert(0) 
+        }
+    })
+}
--- calculator/views.py
+++ calculator/views.py
@@ -1,21 +1,43 @@
 from django.shortcuts import render
-import json
 from django.http import JsonResponse
-from .models import AllIndividualAchievements
 from django.views.decorators.csrf import csrf_exempt
+from django.apps import apps
+import json
+
 
 def calculator_view(request):
+    global all_events
+    all_events = {}
+    table_names = ['HighEconomicShool', 'baumanka', 'mgu', 'mifi', 'mipt', 'FU']
+
+    for table_name in table_names:
+        model = apps.get_model('Individual_achievements', table_name)
+        queryset = model.objects.all()
+        for event in queryset:
+            if 'олимпиад' in event.event:
+                if 'Олимпиады' not in all_events:
+                    all_events.setdefault('Олимпиады', [])
+                all_events['Олимпиады'].append(event.type_of_event.strip())
+            elif 'Другие' in event.event:
+                if 'Другие' not in all_events:
+                    all_events.setdefault('Другие', [])
+                all_events['Другие'].append(event.type_of_event.strip())
+            elif 'Спортивные' in event.event:
+                if 'Спортивные' not in all_events:
+                    all_events.setdefault('Спортивные', [])
+                all_events['Спортивные'].append(event.type_of_event.strip())
+
     return render(request, 'calculator_ID.html')
 
 
+@csrf_exempt
 def get_achievements(request):
-    data = {}
-    for category in AllIndividualAchievements.objects.values_list('category', flat=True).distinct():
-        achievements = AllIndividualAchievements.objects.filter(category=category).values_list('name_event', flat=True)
-        data[category] = list(achievements)
-    return JsonResponse(data)
+    if request.POST:
+        razdel = request.POST.get('razdel')
+        achieve_list = all_events[razdel]
+        return render(request, 'calc_achieves_from_bd.html',  {'achieve_list': achieve_list})
 
-
+        
 @csrf_exempt
 def send_achieves(request):
     if request.method == 'POST':
