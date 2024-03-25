3f33b5c867de8d678b75745345c873f3f154d4a6
Skyber_2 <arttim1019@gmail.com>
2024-03-25 23:46:59 +0300

    change a code for calculator

--- calculator/static/calculator_id/js/calculator_id.js
+++ calculator/static/calculator_id/js/calculator_id.js
@@ -1,79 +1,33 @@
-let selectedAchieves = {}
+const chooseVuzDiv = document.getElementById('choose-vuz')
+const chooseAchievesDiv = document.getElementById('choose-achieves')
 
-function getListAchieves(razdel) {
+function backBtn() {
+    chooseAchievesDiv.style.display = 'none'
+    chooseVuzDiv.style.display = 'block'
+}
+
+
+function chooseVuz(vuzNameRus, vuzNameEng) {
+    h3 = document.getElementById('vuz-name-h3')
+    chooseVuzDiv.style.display = 'none'
+    chooseAchievesDiv.style.display = 'block'
+    h3.innerHTML = vuzNameRus
+    getAchievesFromVuz(vuzNameEng)
+}
+
+
+function getAchievesFromVuz(vuzNameEng) {
     $.ajax({
-        url: '/get_achievements/',
+        url: '/get_achieves_from_vuz/',
         type: 'post',
         data: {
-            'razdel': razdel
-        },  
-        success: function(achieveList) {
-            const sendBtn = document.getElementById('calculate-btn')
-            const achievements = document.getElementById('achievements')
-            sendBtn.style.display = 'block'
-            achievements.innerHTML = ''
-            achievements.innerHTML = achieveList
-            
-            const counter = Number(document.getElementById('counter').textContent)
-            for (let i = 1; i <= counter; i++) {
-                let textAchieve = document.getElementById(`label-achieve-${razdel}${i}`).textContent
-                if (textAchieve in selectedAchieves) {
-                    let checkbox = document.getElementById(`input-achieve-${razdel}${i}`)
-                    let status = document.getElementById(`select-achieve-${razdel}${i}`)
-                    checkbox.checked = true
-                    status.style.display = 'inline-block'
-                    status.value = selectedAchieves[textAchieve]
-                }
-            }
+            'vuz_name': vuzNameEng
+        },
+        success: function(response) {
+            document.getElementById('content_bds').innerHTML = response
         },
         error: function() {
-            // alert(0) 
-        }
-    })
-}
-
-
-function selectAchieve(achieveId) {
-    const inputAchieve = document.getElementById('input-achieve-' + achieveId)
-    const inputActivated = inputAchieve.checked
-    const selectPanel = document.getElementById('select-achieve-' + achieveId)
-
-    inputAchieve.checked = !inputActivated
-    selectPanel.style.display = inputActivated ? 'none' : 'block'
-    actionSelectedAchieve(achieveId, !inputActivated)
-}
-
-
-function actionSelectedAchieve(achieveId, isChecked) {
-    const achieveName = document.getElementById('label-achieve-' + achieveId).textContent
-
-    if (isChecked) {
-        selectedAchieves[achieveName] = 'adding_points_uchastnik'
-    } else {
-        delete selectedAchieves[achieveName]
-    }
-}
-
-
-function changeStatus(achieveId, achieve) {
-    const achieveName = document.getElementById('label-achieve-' + achieveId).textContent
-    selectedAchieves[achieveName] = achieve.value
-}
-
-
-function sendAchieves() {
-    console.log(selectedAchieves)
-    $.ajax({
-        url: '/send_achieves/',
-        type: 'post',
-        data: {
-            'selected_achieves': JSON.stringify(selectedAchieves)
-        },
-        success: function(status) {
-            // alert(status)
-        },
-        error: function(status) {
-            // alert(status)
+            // alert(1)
         }
     })
 }
--- calculator/views.py
+++ calculator/views.py
@@ -7,59 +7,15 @@ import json
 
 
 def calculator_view(request):
-    global all_events
-    all_events = {}
-
-    for table_name in translations:
-        model = apps.get_model('Individual_achievements', table_name)
-        queryset = model.objects.all()
-        for event in queryset:
-            if 'олимпиад' in event.event:
-                if 'Олимпиады' not in all_events:
-                    all_events.setdefault('Олимпиады', [])
-                all_events['Олимпиады'].append(event.type_of_event.strip())
-            elif 'Другие' in event.event:
-                if 'Другие' not in all_events:
-                    all_events.setdefault('Другие', [])
-                all_events['Другие'].append(event.type_of_event.strip())
-            elif 'Спортивные' in event.event:
-                if 'Спортивные' not in all_events:
-                    all_events.setdefault('Спортивные', [])
-                all_events['Спортивные'].append(event.type_of_event.strip())
-
-    return render(request, 'calculator_ID.html')
+    vuzes = [(eng_vuz, rus_vuz) for eng_vuz, rus_vuz in translations.items()]
+    vuzes = sorted(vuzes, key=lambda x: x[1])
+    return render(request, 'calculator_ID.html', {'vuzes': vuzes})
 
 
 @csrf_exempt
-def get_achievements(request):
+def get_achieves_from_vuz(request):
     if request.POST:
-        razdel = request.POST.get('razdel')
-        achieve_list = sorted(set(all_events[razdel]))
-        return render(request, 'calc_achieves_from_bd.html',  {'razdel': razdel, 'achieve_list': achieve_list, 'counter': len(achieve_list)})
-
-        
-@csrf_exempt
-def send_achieves(request):
-    if request.POST:
-        selected_achieves = json.loads(request.POST.get('selected_achieves')) 
-        calculated_for_vuzes = {}
-
-        for vuz, perevod in translations.items():
-            vuz_data = apps.get_model('Individual_achievements', vuz)
-            all_score = 0
-            for name, status in selected_achieves.items():
-                try:
-                    achieve_info = vuz_data.objects.filter(type_of_event=name)
-                except:
-                    pass
-                else:
-                    if achieve_info:
-                        for i in achieve_info:
-                            score = eval(f'i.{status}')
-                            if str(score).isdigit():
-                                all_score += int(score)
-            
-            if all_score:
-                calculated_for_vuzes[perevod] = all_score if all_score <= 10 else 10
-
-        print(calculated_for_vuzes)
+        vuz_name = request.POST.get('vuz_name')
+        all_achieves_from_vuz = apps.get_model('Individual_achievements', vuz_name)
+        list_achives = [queryset for queryset in all_achieves_from_vuz.objects.all()]
+        return render(request, 'achieves_from_vuz.html', {'data': list_achives})
